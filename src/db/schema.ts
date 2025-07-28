45c6544f26de039850c0b3883e4e2e1c7d461b77
Alisher Khamidov <a.a.khamidov@yandex.ru>
2025-07-28 13:27:39 +0200

    add drizzle

---
+++ src/db/schema.ts
@@ -0,0 +1,12 @@
+import { integer, varchar } from "drizzle-orm/pg-core";
+import { pgTable } from "drizzle-orm/pg-core";
+
+export const sportsTable = pgTable("sports", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar({ length: 255 }).notNull().unique(),
+  image: varchar({ length: 255 }).notNull(),
+  description: varchar({ length: 255 }).notNull(),
+});
+
+// npx drizzle-kit push
+// supabase postgres
